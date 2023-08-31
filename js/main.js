bc90de4b81bfa28eed675f39ea5ef6ee317c03eb
Valeria <vmoraleshuaman@gmail.com>
2023-08-31 16:37:28 +0930

    class 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
 console.log("Hello World");
 
 console.log(2+5);
+
+// let firstPrize = 299;
+// const companyName = "CLG";
+// let greeting = "Hello";
+// let morning = true;
+
+// console.log(morning);
+// console.log(firstPrize);
+// console.clear();
+
+// let firstWinner = "Grace";
+
+// console.log("The winner is " + firstWinner);
+
+// console.log(`The winner is still ${firstWinner}`);
+
+// let winnerAnnouncement = `The winner is still ${firstWinner}`;
+
+// let result = winnerAnnouncement.toUpperCase();
+
+// console.log(result);
+
+// console.log(`${result.length}`);
+
+// let resultLength = result.length;
+// console.log(resultLength);
+
+let brandingProjects = 10;
+let featuredProject = "AKD Professional";
+let gifsProjects = null;
+let showFeatureProject = true;
+
+console.log(featuredProject);
+
+console.log("We have " + brandingProjects + " Branding Projects");
+
+console.clear();
+
+console.warn("No more Branding Projects");
+
+console.error("Zero Branding Projects")
+
+let project = featuredProject.toLowerCase();
+
+console.log(project);
